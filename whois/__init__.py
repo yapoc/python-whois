223466a5554d18be74389fd4204dc28559519937
yapoc <yapspam@yapoc.org>
2017-10-26 16:12:26 +0200

    Indentation + passage en socket + suppression du paramètre ignore_returncode

--- whois/__init__.py
+++ whois/__init__.py
@@ -31,7 +31,7 @@ class WhoisException (Exception):
   pass
 
 
-def query(domain, force=0, cache_file=None, slow_down=0, ignore_returncode=0):
+def query(domain, force=0, cache_file=None, slow_down=0):
   """
     force=1        <bool>    Don't use cache.
     cache_file=<path>  <str>    Use file to store cache not only memory.
@@ -51,7 +51,7 @@ def query(domain, force=0, cache_file=None, slow_down=0, ignore_returncode=0):
   if tld not in TLD_RE.keys(): raise Exception('Unknown TLD: %s\n(all known TLD: %s)' % (tld, list(TLD_RE.keys())))
 
   while 1:
-    pd = do_parse(do_query(d, force, cache_file, slow_down, ignore_returncode), tld)
+    pd = do_parse(do_query(d, force, cache_file, slow_down), tld)
     if (not pd or not pd['domain_name'][0]) and len(d) > 2: d = d[1:]
     else: break
 
